a8525801f4e0ad0e232978f2a4dde19bd3b4001d
leisong1 <leisong1@teach.cs.utoronto.ca>
2026-05-28 23:20:33 -0700

    added type checks to api main.py

--- fps-regression-api/app/main.py
+++ fps-regression-api/app/main.py
@@ -1,6 +1,7 @@
 import joblib
 import numpy as np
 import pandas as pd
+from typing import Literal
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
@@ -9,8 +10,12 @@ app = FastAPI(title="FPS Prediction API")
 preprocessor = joblib.load("model/preprocessor.joblib")
 models = joblib.load("model/models.joblib")
 
+genres = ['Adventure', 'Brawler', 'Indie', 'Puzzle', 'RPG', 
+            'Racing', 'Real Time Strategy', 'Shooter', 'Simulator', 
+            'Sport', 'Strategy', 'Tactical', 'Turn Based Strategy']
+
 class InputData(BaseModel):
-    Preset: str
+    Preset: Literal['low', 'medium', 'high', 'ultra']
     boostClock: int
     shaders: int
     tmus: int
@@ -20,19 +25,19 @@ class InputData(BaseModel):
     memoryBandwidth: int
     Resolution: int
     Release_Date: int
-    Adventure: int
-    Brawler: int
-    Indie: int
-    Puzzle: int
-    RPG: int
-    Racing: int
-    RTS: int = Field(alias="Real Time Strategy")
-    Shooter: int
-    Simulator: int
-    Sport: int
-    Strategy: int
-    Tactical: int
-    TBS: int = Field(alias="Turn Based Strategy")
+    Adventure: bool
+    Brawler: bool
+    Indie: bool
+    Puzzle: bool
+    RPG: bool
+    Racing: bool
+    RTS: bool = Field(alias="Real Time Strategy")
+    Shooter: bool
+    Simulator: bool
+    Sport: bool
+    Strategy: bool
+    Tactical: bool
+    TBS: bool = Field(alias="Turn Based Strategy")
 
 @app.get("/")
 def home():
@@ -41,6 +46,7 @@ def home():
 @app.post("/predict")
 def predict_fps(data: InputData):
     input_df = pd.DataFrame([data.model_dump(by_alias=True)])
+    input_df[genres] = input_df[genres].astype(int)
     input_processed = preprocessor.transform(input_df)
     
     predictions = []
